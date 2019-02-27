a501f2ec67aad1c97d3803bff194b4ac21992da2
alexandra <alexaspalato@gmail.com>
2019-02-27 07:11:36 +0100

    Strip PostEntryMeta

--- src/components/PostEntryMeta/index.js
+++ src/components/PostEntryMeta/index.js
@@ -1,25 +1,19 @@
-import React from 'react'
+import React from "react"
 import moment from "moment/moment"
-import { Row, Col, Avatar } from "antd"
 import { Link } from "gatsby"
 
 const PostEntryMeta = ({ post }) => (
-  <Row
-    justify="middle"
-    style={{ textAlign: `center`, marginBottom: `15px` }}
-    gutter={16}
-  >
-    <Col xs={24} md={24} style={{ textAlign: `center` }}>
-      <Link to={`/author/${post.author.slug}`}>
-        <Avatar size={100} src={post.author.avatar.url} />
-      </Link>
-    </Col>
-    <Col xs={24} md={24} style={{ textAlign: `center` }}>
-      <Link to={`/author/${post.author.slug}`}>{post.author.name}</Link>
-      <br />
-      {moment(post.date).format(`MMM Do YY`)}
-    </Col>
-  </Row>
+  <>
+    <Link to={`/author/${post.author.slug}`}>
+      <div className="avatar">
+        <img src={post.author.avatar.url} alt="" />
+      </div>
+    </Link>
+
+    <Link to={`/author/${post.author.slug}`}>{post.author.name}</Link>
+    <br />
+    {moment(post.date).format(`MMM Do YY`)}
+  </>
 )
 
 export default PostEntryMeta

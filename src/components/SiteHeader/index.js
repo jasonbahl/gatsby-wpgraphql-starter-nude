222bac193b09ef4306728edf8901de4ebaced927
alexandra <alexaspalato@gmail.com>
2019-02-27 08:24:30 +0100

    strip Header

--- src/components/SiteHeader/index.js
+++ src/components/SiteHeader/index.js
@@ -1,11 +1,8 @@
 import { Link, StaticQuery, graphql } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
-import { Layout } from "antd"
 import SiteMenu from "../SiteMenu"
 import wpgraphqlLogo from "../../images/wpgraphql-logo.png"
-
-const { Header } = Layout
 
 const Index = ({ location }) => (
   <StaticQuery
@@ -19,35 +16,19 @@ const Index = ({ location }) => (
       }
     `}
     render={data => (
-      <Header>
-        <div
-          className="logo"
-          style={{
-            minWidth: `120px`,
-            minHeight: `31px`,
-            lineHeight: `31px`,
-            margin: `16px 24px 16px 0`,
-            float: `left`,
-          }}
-        >
-          <Link
-            to="/"
-            style={{
-              color: `white`,
-              textDecoration: `none`,
-            }}
-          >
+      <div className="header">
+        <div className="logo">
+          <Link to="/">
             <img
               src={wpgraphqlLogo}
               alt="WPGraphQL Logo"
               style={{ height: `30px`, width: `30px` }}
             />
-            {` `}
             {data.site.siteMetadata.title}
           </Link>
         </div>
         <SiteMenu location={location} />
-      </Header>
+      </div>
     )}
   />
 )

64205336c991659459819a546cd73731d9ce6734
alexandra <alexaspalato@gmail.com>
2019-02-27 07:17:54 +0100

    Strip page template

--- src/templates/page.js
+++ src/templates/page.js
@@ -1,6 +1,5 @@
 import React from "react"
 import { graphql } from "gatsby"
-import { Row, Col, Divider } from "antd"
 import SiteLayout from "../components/SiteLayout"
 import CategoriesWidget from "../components/CategoriesWidget"
 import RecentCommentsWidget from "../components/RecentCommentsWidget"
@@ -18,25 +17,17 @@ const Page = props => {
   return (
     <SiteLayout location={location}>
       <Seo title={`${page.title}`} />
-      <Row type="flex" gutter={24}>
-        <Col xs={24} md={16}>
-          <h1 style={{ wordBreak: `break-all` }}>{title}</h1>
-          <Divider />
-          <Row type="flex" justify="space-around" gutter={24}>
-            <Col xs={24}>
-              <div
-                style={{ wordBreak: `break-all` }}
-                dangerouslySetInnerHTML={{ __html: content }}
-              />
-            </Col>
-          </Row>
-        </Col>
-        <Col xs={24} md={8}>
-          <RecentPostsWidget />
-          <CategoriesWidget />
-          <RecentCommentsWidget />
-        </Col>
-      </Row>
+
+      <h1 style={{ wordBreak: `break-all` }}>{title}</h1>
+
+      <div
+        style={{ wordBreak: `break-all` }}
+        dangerouslySetInnerHTML={{ __html: content }}
+      />
+
+      <RecentPostsWidget />
+      <CategoriesWidget />
+      <RecentCommentsWidget />
     </SiteLayout>
   )
 }
